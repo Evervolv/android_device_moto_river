d6d92124866098b6821e1239958c8c8002c85477
Erfan Abdi <erfangplus@gmail.com>
2019-10-16 00:25:34 +0330

    river: Add ev overlays
    Change-Id: I45564c4782e27364d4a4281e6504b882a76b89d6

--- device.mk
+++ device.mk
@@ -19,8 +19,8 @@ $(call inherit-product, vendor/moto/river/river-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
-PRODUCT_ENFORCE_RRO_TARGETS := *
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-ev
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
