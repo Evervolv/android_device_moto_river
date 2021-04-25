LineageOS/android_device_motorola_river

river: Don't let builds complete without vendor tree
* There is zero reason to ever build without blobs. We've even seen
  this situation with official builds from our servers. It's always
  better for a build to fail than it is for it to produce something
  that has no chance at working.

Change-Id: I4968795670c91f691e9ecdc0e4af62e16ba3a93a

device.mk
@@ -15,7 +15,7 @@
 #
 
 # Vendor blobs
-$(call inherit-product-if-exists, vendor/motorola/river/river-vendor.mk)
+$(call inherit-product, vendor/motorola/river/river-vendor.mk)
 
 # Properties
 -include $(LOCAL_PATH)/vendor_prop.mk

Evervolv/android_device_moto_river

device.mk
@@ -15,7 +15,7 @@
 #
 
 # Vendor blobs
-$(call inherit-product-if-exists, vendor/moto/river/river-vendor.mk)
+$(call inherit-product, vendor/moto/river/river-vendor.mk)
 
 # Properties
 -include $(LOCAL_PATH)/vendor_prop.mk