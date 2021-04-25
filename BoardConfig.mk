LineageOS/android_device_motorola_river

river: Make vendor board-configuration mandatory
Change-Id: I2cb9aba5f858c845399aa80afdbbf97edfd801dd

BoardConfig.mk
@@ -40,4 +40,4 @@ BOARD_VENDORIMAGE_PARTITION_SIZE := 861507584     #   841316 * 1024 mmcblk0p60-6
 PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
 
 # inherit from the proprietary version
--include vendor/motorola/river/BoardConfigVendor.mk
+include vendor/motorola/river/BoardConfigVendor.mk

Evervolv/android_device_moto_river

BoardConfig.mk
@@ -37,4 +37,4 @@ BOARD_VENDORIMAGE_PARTITION_SIZE := 861507584     #   841316 * 1024 mmcblk0p60-6
 PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
 
 # inherit from the proprietary version
--include vendor/moto/river/BoardConfigVendor.mk
+include vendor/moto/river/BoardConfigVendor.mk