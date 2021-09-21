fb0931bf86466e70b5fb8209adec7191a58a5ce3
Nolen Johnson <johnsonnolen@gmail.com>
2021-09-20 20:40:16 -0400

    river: Correct product values to reflect stock
    Change-Id: I2e930c2be4c71c447316f7a2bc1034771cc04714

--- ev_river.mk
+++ ev_river.mk
@@ -35,8 +35,8 @@ PRODUCT_NAME := ev_river
 PRODUCT_MODEL := moto g(7)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-        PRODUCT_NAME=river \
-        PRIVATE_BUILD_DESC="river-user 9 PPOS29.114-134-4 fe214 release-keys"
+        PRODUCT_NAME=river_retail \
+        PRIVATE_BUILDS_DESC="river_retail-user 10 QPUS30.52-33-11 df129 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := motorola/river_retail/river:10/QPUS30.52-33-11/df129:user/release-keys
