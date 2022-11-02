LineageOS/android_device_motorola_river

river: Add board-info.txt
* Fixes flashall in fastbootd

Change-Id: Iae2f33113317cfaadeff7a3610463880077c35e0

BoardConfig.mk
@@ -20,6 +20,7 @@
 DEVICE_PATH := device/motorola/river
 
 # Assertions
+TARGET_BOARD_INFO_FILE := device/motorola/river/board-info.txt
 TARGET_OTA_ASSERT_DEVICE := river
 
 # Display

Evervolv/android_device_moto_river

BoardConfig.mk
@@ -20,6 +20,7 @@
 DEVICE_PATH := device/moto/river
 
 # Assertions
+TARGET_BOARD_INFO_FILE := device/moto/river/board-info.txt
 TARGET_OTA_ASSERT_DEVICE := river
 
 # Kernel